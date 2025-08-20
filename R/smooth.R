f3f8ea4f5998176a5da254042cfcc30ba2695a2c
Eric Tytell <eric.tytell@tufts.edu>
2025-08-20 14:38:40 -0400

    Added more documentation

--- R/smooth.R
+++ R/smooth.R
@@ -6,7 +6,10 @@
 #' @param .data Data frame containing the midlines.
 #' @param cols Columns containing the components to be smoothed. Often these will
 #'   be the x and y coordinates of the midline.
-#' @param .out Names of the output columns. Currently ignores this parameter
+#' @param .out Names of the output columns. Should either be a list with the same
+#'   number of elements as cols, or a glue specification as in `dplyr::across` for
+#'   the `.names` parameter. The default (NULL) means that the output columns will
+#'   have the same name as the original column with an 's' appended at the end.
 #' @param .frame,.point Columns identifying frames and points (defaults are `frame`
 #'   and `point`)
 #' @param fillgaps Longest gap to interpolate over. default is 0, which means not
@@ -49,6 +52,7 @@ smooth_points_df <- function(
     .point <- enquo(.point)
   }
 
+  # only look for gaps if they want us to fill them
   if (fillgaps > 0) {
     gapdata <- .data |>
       find_gaps_df({{ cols }}, .frame = {{ .frame }})
@@ -57,16 +61,44 @@ smooth_points_df <- function(
       mutate(gaplen = 0)
   }
 
+  if (is.null(.out)) {
+    nms <- '{.col}s'
+  } else {
+    assertthat::assert_that(
+      (length(.out) == 1) ||
+        length(.out) == length(cols),
+      msg = "`.out` must either have the same length as `cols` or must have a name spec suitable for use in `dplyr::across` in the `.names` parameter"
+    )
+    nms <- .out
+  }
+
   gapdata |>
     group_by({{ .point }}) |>
     mutate(across(
       {{ cols }},
       \(y) smooth_point({{ .frame }}, y, gaplen <= fillgaps, spar = spar),
-      .names = '{.col}s'
+      .names = nms
     ))
 }
 
-find_gaps_df <- function(.data, cols, .frame = frame) {
+
+#' Find gaps in a data series
+#'
+#' Looks for NAs (gaps) in a data series and counts the number of NAs.
+#' Useful for `smooth_points_df`, which can fill gaps up to a certain length.
+#' 0 means no gap, 1 means a single NA, and so forth.
+#'
+#' @param .data Data frame containing the midlines.
+#' @param cols Columns containing the components to be smoothed. Often these will
+#'   be the x and y coordinates of the midline.
+#' @param .frame Column identifying frames (default is `frame`)
+#' @param .out Name of the column to contain the length of the gap.
+#'
+#' @returns The data frame with a new column.
+#' @export
+#'
+#' @examples
+find_gaps_df <- function(.data, cols, .frame = frame, .out = c('gaplen')) {
   gaps <- .data |>
     mutate(across({{ cols }}, is.na)) |>
     mutate(good = (rowSums(pick({{ cols }})) == 0))
@@ -88,11 +120,26 @@ find_gaps_df <- function(.data, cols, .frame = frame) {
   return(.data)
 }
 
-smooth_point <- function(x, y, goodout, spar) {
+#' Applies a smoothing spline to a data series, potentially with gaps
+#'
+#' Builds a smoothing spline for y(x), where `y` may contain gaps (NAs).
+#' Ignores the NAs. The uses the spline to interpolate values at the
+#' same `x` coordinates, potentially filling in the gaps.
+#'
+#' @param x x coordinate
+#' @param y y coordinate, potentially with NAs
+#' @param goodout Logical vector of where in the x coordinate to interpolate
+#' @param spar Smoothing parameter (see `smooth.spline`)
+#'
+#' @returns The smoothed values
+#' @export
+#'
+#' @examples
+smooth_point <- function(x, y, goodout = NULL, spar) {
   ys <- numeric(length(y))
   good <- !is.na(y)
 
-  if (missing(goodout)) {
+  if (missing(goodout) || is.null(goodout)) {
     goodout <- good
   }
 
