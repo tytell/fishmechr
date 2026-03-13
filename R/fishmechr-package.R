9d6c1106b5b79dd62cb6ee4dfc5cdd74a39bb510
Eric Tytell <eric.tytell@tufts.edu>
2026-03-13 10:25:46 -0400

    Add importFrom for dplyr, rlang, and tidyr functions used throughout package

--- R/fishmechr-package.R
+++ R/fishmechr-package.R
@@ -2,5 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr across any_of bind_cols case_when filter first group_by
+#'   if_else is_grouped_df lag lead left_join mutate n pick rename row_number
+#'   select summarise summarize ungroup
+#' @importFrom rlang as_name enquo eval_tidy quo_name
+#' @importFrom tidyr fill unnest
 ## usethis namespace: end
 NULL
